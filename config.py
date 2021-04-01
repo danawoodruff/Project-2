fd089fe93351d54fe5faa25dcff68eb68c03fbfb
krla.20 <kmontero1920@gmail.com>
2021-03-31 21:44:33 -0500

    updating to heroku deployment

---
+++ config.py
@@ -0,0 +1,18 @@
+from secrets import username, password
+
+# rds_connection_string = f"{username}:{password}@localhost:5432/etl_team5"
+# engine = create_engine(f'postgresql://{rds_connection_string}')
+
+# database = "postgres"
+
+database = "heroku"
+
+connect_string = ""
+
+if database == "postgres":
+    # connect_string = "postgresql://username:password@localhost:5432/etl_team5"
+
+    connect_string = f"postgres://{username}:{password}@localhost:5432/etl_team5"
+    
+elif database == "heroku":        
+    connect_string = " postgres://uothqbtdpaanvg:2fde20bbd35347df59d70e1177499cff2eb72b2629941b7c82a071fe36677ec0@ec2-100-24-139-146.compute-1.amazonaws.com:5432/d6rafo5fo1kucb"
